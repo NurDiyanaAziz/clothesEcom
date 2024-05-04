db2e28a5f816adace4b679a3e6a3dd73f61354e0
NurDiyanaAziz <nurdiyanaabaziz26@gmail.com>
2024-05-04 15:20:42 +0800

    update1

---
+++ project/src/main/java/com/clothesEcom/project/EClothesEcomProjectApplication.java
@@ -0,0 +1,13 @@
+package com.clothesEcom.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EClothesEcomProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EClothesEcomProjectApplication.class, args);
+	}
+
+}
---
+++ project/src/test/java/com/clothesEcom/project/EClothesEcomProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clothesEcom.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EClothesEcomProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
